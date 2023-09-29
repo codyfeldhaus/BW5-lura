ecd79fe27e517cf9852fdb9b56c63c0d37deadb4
ludiaz978 <142175671+ludiaz978@users.noreply.github.com>
2023-09-28 18:44:13 -0700

    WIP

---
+++ react-demo/src/App.jsx
@@ -0,0 +1,13 @@
+import <HelloWorld  from './HelloWorld';
+import <ClickCounter from './ClickCounter';
+import './App.css';
+function App() {
+return (
+<div>
+<h1>React DEmos 9/27</h1>
+<HelloWorld/>
+<ClickCounter/>
+</div>
+);
+}
+export default App;
